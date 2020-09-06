3e8ebe19c66215cee7025c48449f8ac2422a08c9
Jackson <Jackson.harris3@gmail.com>
2020-09-06 00:22:22 -0700

    update routing for ipfs

--- next.config.js
+++ next.config.js
@@ -3,19 +3,29 @@ const withFonts = require('next-fonts');
 const withImages = require('next-images');
 const withPlugins = require('next-compose-plugins');
 
-module.exports = withPlugins([withCSS({
-	webpack: (config, { isServer }) => {
-		// Fixes npm packages that depend on `fs` module
-		if (!isServer) {
-			config.node = {
-				fs: 'empty',
-      };
-    }
-    config.module.rules.push({
-			test: /.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-			use: { loader: 'url-loader', options: { limit: 100000, name: '[name].[ext]' } },
-		});
+module.exports = withPlugins([
+	withCSS({
+		webpack: (config, { isServer }) => {
+			// Fixes npm packages that depend on `fs` module
+			if (!isServer) {
+				config.node = {
+					fs: 'empty',
+				};
+			}
+			config.module.rules.push({
+				test: /.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+				use: { loader: 'url-loader', options: { limit: 100000, name: '[name].[ext]' } },
+			});
 
-		return config;
-	},
-}), withFonts, withImages]);
+			return config;
+		},
+		exportTrailingSlash: true,
+		exportPathMap: function () {
+			return {
+				'/': { page: '/' },
+			};
+		},
+	}),
+	withFonts,
+	withImages,
+]);
